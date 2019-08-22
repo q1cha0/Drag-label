2a4259319e714aef2e3f0af147ab673b0e7896e4
q1cha0 <cqc168@live.com>
2019-08-22 20:05:11 +0800

    update functions

--- src/components/leaflet-demo.vue
+++ src/components/leaflet-demo.vue
@@ -15,31 +15,31 @@
       };
     },
     mounted() {
-      let map = L.map('map', {
-        crs: L.CRS.Simple
+      // 扩展 marker rect 类
+      let MyIconRect = L.DivIcon.extend({
+        options: {
+          className: 'my-div-icon-rect',
+          iconSize: [80, 20],
+          iconAnchor: [85, 10]
+        }
       });
-      let bounds = [[0, 0], [4.22, 7.50]];
-      L.imageOverlay('machine-view-overview.png', bounds).addTo(map);
-      // map.setView([2.11, 3.75], 6); // 中心点，scale
-      map.fitBounds(bounds);
-      // L.control.layers
-
-      let myIconRect_1 = L.divIcon({
-        className: 'my-div-icon-rect',
+      // 具体的实例配置
+      // label Rect 的
+      let myIconRect_1 = new MyIconRect({
         html: `<span>${ this.markerData }</span>`,
-        iconSize: [80, 20],
-        iconAnchor: [80, 10]
       });
+      // label circle 的
       let myIconCircle_1 = L.divIcon({
         className: 'my-div-icon-circle',
       });
       // you can set .my-div-icon styles in CSS
       // L.marker([50.505, 30.57], { icon: myIcon }).addTo(map);
 
+      // 绘制牵引线
       let linkLine_latlngs_1 = [ // lat:维度-Y lng:经度-X
-        [1, 1],
-        [1, 2],
-        [3, 2]
+        [100, 100],
+        [100, 200],
+        [300, 200]
       ];
       let linkLine_1 = L.polyline(linkLine_latlngs_1, {
         color: 'red',
@@ -48,8 +48,8 @@
       // zoom the map to the polyline
       // map.fitBounds(polyline.getBounds());
 
-      // 添加信息矩形
-      let labelRect_1 = L.marker([1, 1], {
+      // 绘制信息矩形及其事件
+      let labelRect_1 = L.marker([100, 100], {
         draggable: true,
         icon: myIconRect_1,
       }).bindPopup('真名1');
@@ -58,68 +58,121 @@
         let autoXLng = undefined;
         let autoYLat = undefined;
         let curLatlng = e.target._latlng;
-        let xLng = (curLatlng.lng).toFixed(6);
-        let yLat = (curLatlng.lat).toFixed(6);
+        let xLng = curLatlng.lng;
+        let yLat = curLatlng.lat;
         console.log(xLng, yLat);
-        let xLngFrac = Number(xLng.split('.')[1][0]);
-        let yLngFrac = Number(yLat.split('.')[1][0]);
-        if (xLngFrac <= 5 && yLngFrac <= 5) {
+        // let xLngFrac = Number(xLng.split('.')[1][0]);
+        // let yLngFrac = Number(yLat.split('.')[1][0]);
+        if (xLng % 50 <= 50 && yLat % 50 <= 50) {
           autoXLng = Math.floor(Number(xLng));
           autoYLat = Math.floor(Number(yLat));
-        } else if (xLngFrac <= 5 && yLngFrac > 5) {
+        } else if (xLng % 50 <= 50 && yLat % 50 > 50) {
           autoXLng = Math.floor(Number(xLng));
           autoYLat = Math.ceil(Number(yLat));
-        } else if (xLngFrac > 5 && yLngFrac <= 5) {
+        } else if (xLng % 50 > 50 && yLat % 50 <= 50) {
           autoXLng = Math.ceil(Number(xLng));
           autoYLat = Math.floor(Number(yLat));
-        } else if (xLngFrac > 5 && yLngFrac > 5) {
+        } else if (xLng % 50 > 50 && yLat % 50 > 50) {
           autoXLng = Math.ceil(Number(xLng));
           autoYLat = Math.ceil(Number(yLat));
         }
         labelRect_1.setLatLng([autoYLat || yLat, autoXLng || xLng]);
 
+        // 重置牵引线的坐标
         linkLine_latlngs_1[0] = [autoYLat || yLat, autoXLng || xLng];
         linkLine_1.setLatLngs(linkLine_latlngs_1);
 
       });
-      // 指示圆形
-      let labelCircle_1 = L.marker([3, 2], {
+      // 标记圆形 及其事件
+      let labelCircle_1 = L.marker([300, 200], {
         draggable: true,
         icon: myIconCircle_1
       });
       labelCircle_1.on('dragend', e => {
         console.log(e.target._latlng);
+        // 更新牵引线坐标
         linkLine_latlngs_1[2] = e.target._latlng;
         linkLine_1.setLatLngs(linkLine_latlngs_1);
 
+        // test 更新icon中的信息
         this.markerData = '别名' + (Math.random() * 100).toFixed(0);
         labelRect_1.setIcon(
-          L.divIcon({
-            className: 'my-div-icon-rect',
-            html: `<span>${ this.markerData }</span>`,
-            iconSize: [80, 20],
-            iconAnchor: [80, 10]
+          new MyIconRect({
+            html: `<span>${ this.markerData }</span>`
           })
         );
       });
 
-      let labelRect_2 = L.marker([4, 6]).bindPopup('This is Denver, CO.');
-
-      let cities = L.layerGroup([labelRect_1, labelCircle_1, labelRect_2, linkLine_1]);
+      let labelRect_2 = L.marker([400, 600]).bindPopup('This is Denver, CO.');
+      let markerLayer_1 = L.layerGroup([labelRect_1, labelCircle_1, linkLine_1, labelRect_2]);
 
       // test
       let littleton1 = L.marker([0, 1]).bindPopup('This is Littleton, CO.');
       let denver1 = L.marker([1, 2]).bindPopup('This is Denver, CO.');
       let aurora1 = L.marker([2, 3]).bindPopup('This is Aurora, CO.');
       let golden1 = L.marker([3, 4]).bindPopup('This is Golden, CO.');
-      let cities1 = L.layerGroup([littleton1, denver1, aurora1, golden1]);
+      let markerLayer_2 = L.layerGroup([littleton1, denver1, aurora1, golden1]);
 
-      let overlayMaps = {
-        'Cities': cities,
-        'Cities1': cities1
+      let overlayLabels = {
+        'Markers_1': markerLayer_1,
+        'Markers_2': markerLayer_2
       };
 
-      L.control.layers(undefined, overlayMaps).addTo(map);
+      let latlngBounds = [[0, 0], [338, 600]]; // 经纬度边界
+      let map = L.map('map', {
+        maxBounds: latlngBounds,
+        crs: L.CRS.Simple,
+        dragging: false,
+        // minZoom: 7,
+        // maxZoom: 7,
+        // center: [0, 0],
+        layers: [markerLayer_1] // 初始化默认显示的layer
+      });
+      // map.setView([0, 0], 0); // 中心点，scale
+      map.fitBounds(latlngBounds);
+      L.imageOverlay('machine-view-overview.png', latlngBounds).addTo(map);
+      L.control.layers(null, overlayLabels).addTo(map);
+
+      // 刻度线层
+      L.GridLayer.AutoAlignLine = L.GridLayer.extend({
+        createTile: function () {
+          let tile = document.createElement('div');
+          let size = this.getTileSize();
+          tile.style.outline = '1px solid #999';
+          tile.width = size.x;
+          tile.height = size.y;
+          return tile;
+        }
+      });
+      L.gridLayer.autoAlignLine = function (opts) {
+        return new L.GridLayer.AutoAlignLine(opts);
+      };
+
+      map.addLayer(
+        L.gridLayer
+          .autoAlignLine(
+            {
+              tileSize: 50,
+              pane: 'overlayPane',
+              opacity: 0.3
+            }
+          )
+      );
+
+      // map.on('click', e => {
+      //   L.popup()
+      //     .setLatLng(e.latlng)
+      //     .setContent("You clicked the map at " + e.latlng.toString())
+      //     .openOn(map);
+      // });
+
+      // check some info
+      // setTimeout(() => {
+      //   console.log(map.getSize());
+      // }, 2000);
+      // console.log(map.getSize());
+      // console.log(map.getBounds());
+      // console.log(map.getPixelBounds());
 
     },
     methods: {
@@ -131,8 +184,8 @@
 </script>
 <style>
   #map {
-    /*width: 400px;*/
-    height: 400px;
+    width: 600px;
+    height: 600px;
   }
 
   .my-div-icon-rect {
